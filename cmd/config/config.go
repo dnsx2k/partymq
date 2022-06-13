f4373dea4c007936b837f47d65487f3675ae085d
Mateusz Pasterny <92987016+dnsx2k@users.noreply.github.com>
2022-06-13 16:38:15 +0200

    feat: default values for NoConsumerTimeout and LogSeverity

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 type Config struct {
-	RabbitMqConnectionString string  `envconfig:"partymq_rabbit_cs" required:"true"`
-	PartitionKey string `envconfig:"partymq_partition_key" default:"id"`
-	HealthCheckInterval string
-	NoConsumerTimeout string
+	RabbitMqConnectionString string `envconfig:"partymq_rabbit_cs" required:"true"`
+	PartitionKey             string `envconfig:"partymq_partition_key" default:"id"`
+	HealthCheckInterval      string `envconfig:"partymq_health_check_interval" default:"30s"`
+	NoConsumerTimeout        string `envconfig:"partymq_no_consumer_timeout" default:"10s"`
+	LogSeverity              string `envconfig:"partymq_log_severity" default:"debug"`
 }
