1a1bd3f072cf32959db61f08cabeb18d3569981f
Mateusz Pasterny <92987016+dnsx2k@users.noreply.github.com>
2022-06-13 16:43:14 +0200

    refactor: changed way of holding rabbit's connections, handling connection close

--- pkg/rabbit/rabbit.go
+++ pkg/rabbit/rabbit.go
@@ -18,106 +18,94 @@ type AmqpOrchestrator interface {
 }
 
 type amqpCtx struct {
-	primaryConnection   *amqp.Connection
-	publishConnection   *amqp.Connection
-	subscribeConnection *amqp.Connection
+	connections map[Direction]*amqp.Connection
 }
 
-// TODO: Notify connection close
+// Init - initializes amqp connections
 func Init(url string) (AmqpOrchestrator, error) {
-	actx := amqpCtx{}
+	notifyCloseCh := make(chan *amqp.Error)
+	actx := amqpCtx{
+		connections: make(map[Direction]*amqp.Connection),
+		logger:      nil,
+	}
 	primConn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	actx.primaryConnection = primConn
+	actx.connections[DirectionPrimary] = primConn
+	primConn.NotifyClose(notifyCloseCh)
 
 	subConn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	actx.subscribeConnection = subConn
+	actx.connections[DirectionSub] = subConn
+	subConn.NotifyClose(notifyCloseCh)
 
 	pubConn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	actx.publishConnection = pubConn
+	actx.connections[DirectionPub] = pubConn
+	subConn.NotifyClose(notifyCloseCh)
+
+	go actx.handleConnectionClose(notifyCloseCh)
 
 	return &actx, nil
 }
 
+// CreateResources - creates amqp resources
 func (ac *amqpCtx) CreateResources() error {
-	ch, err := ac.primaryConnection.Channel()
+	ch, err := ac.connections[DirectionPrimary].Channel()
 	if err != nil {
 		return err
 	}
+	defer ch.Close()
 
 	if err = ch.ExchangeDeclare(PartyMqExchange, amqp.ExchangeDirect, false, true, false, false, nil); err != nil {
 		return err
 	}
-
 	if _, err = ch.QueueDeclare(PartyMqBufferQueue, false, false, false, false, nil); err != nil {
-		return err
-	}
-
-	if err = ch.Close(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+// InitPartition - initializes new partition for client, returns name of created and bounded queue
 func (ac *amqpCtx) InitPartition(clientID string) (string, error) {
-	ch, err := ac.primaryConnection.Channel()
+	ch, err := ac.connections[DirectionPrimary].Channel()
 	if err != nil {
 		return "", err
 	}
+	defer ch.Close()
 
-	queue, err := ch.QueueDeclare(fmt.Sprintf("party-mq-partition-%s", clientID), false, false, false, false, nil)
-	if err != nil{
+	queue, err := ch.QueueDeclare(fmt.Sprintf("partymq.q.partition-%s", clientID), false, false, false, false, nil)
+	if err != nil {
 		return "", err
 	}
-
-	err = ch.QueueBind(queue.Name, fmt.Sprintf("party-mq-partition-key-%s", clientID), PartyMqExchange, false, nil)
-	if err != nil{
-		// TODO: Delete declared queue "teardown"
-		return "", nil
+	err = ch.QueueBind(queue.Name, fmt.Sprintf("partymq.r.partition-%s", clientID), PartyMqExchange, false, nil)
+	if err != nil {
+		if _, err := ch.QueueDelete(queue.Name, false, false, false); err != nil {
+			return "", err
+		}
 	}
 
 	return queue.Name, nil
 }
 
+// GetChannel - based on passed direction create new amqp channel
 func (ac *amqpCtx) GetChannel(d Direction) (*amqp.Channel, error) {
-	switch d {
-	case DirectionSub:
-		ch, err := ac.subscribeConnection.Channel()
-		if err != nil {
-			return nil, err
-		}
-		// TODO: Channel close notify
-		return ch, nil
-	case DirectionPub:
-		ch, err := ac.publishConnection.Channel()
-		if err != nil {
-			return nil, err
-		}
-		// TODO: Channel close notify
-		return ch, nil
-	case DirectionPrimary:
-		ch, err := ac.primaryConnection.Channel()
-		if err != nil {
-			return nil, err
-		}
-		// TODO: Channel close notify
-		return ch, nil
-	default:
-		// TODO: Return some error
-		return nil, nil
+	ch, err := ac.connections[d].Channel()
+	if err != nil {
+		return nil, err
 	}
+
+	return ch, nil
 }
 
-func (ac *amqpCtx)GetMessageTemplate() amqp.Publishing{
+// GetMessageTemplate - returns amqp msg boilerplate
+func (ac *amqpCtx) GetMessageTemplate() amqp.Publishing {
 	return amqp.Publishing{
 		Headers:         nil,
 		ContentType:     "",
@@ -148,4 +136,11 @@ func (ac *amqpCtx)InspectQueues(m map[uuid.UUID]party.Client) (map[uuid.UUID]amq
 		out[k] = q
 	}
 	return out, nil
+}
+
+func (ac *amqpCtx) handleConnectionClose(c <-chan *amqp.Error) {
+	for {
+		err := <-c
+		ac.logger.Error(err.Error())
+	}
 }
