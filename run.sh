78d521058e27fa0b79f33cf4d7dff9697d4b217b
Mateusz Pasterny <mpasterny@g2a.com>
2023-11-30 16:31:03 +0100

    Test trigger by /run HTTP endpoint, integration tests flow adjusted to Github actions

---
+++ run.sh
@@ -0,0 +1,1 @@
+curl -X POST http://0.0.0.0:8080/run
--- tests/main.go
+++ tests/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/dnsx2k/partymq/tests/pkg/scenarios"
 	"github.com/dnsx2k/partymq/tests/pkg/test"
 	"github.com/dnsx2k/partymq/tests/pkg/utils"
+	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,10 +20,6 @@ var testScenarios = map[string]utils.Scenario{
 }
 
 func main() {
-	// TODO: Replace with partymq healthcheck configured in docker-compose file
-	fmt.Println("Sleep...")
-	time.Sleep(15 * time.Second)
-	ctx := context.Background()
 	amqpCs := os.Getenv("RABBIT_CS")
 
 	conn, err := amqp.Dial(amqpCs)
@@ -34,16 +33,32 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 	utils.InitEmitterResources(ch)
-	for k, v := range testScenarios {
-		utils.Seed(ctx, ch, v.MessageVolume, v.IdPoolSize)
 
-		start := time.Now()
-		v.ExecuteScenarioFn(ch)
-		fmt.Printf("Executed scenario: %s, with %v messages and %v of ID pool size. In %v", k, v.MessageVolume, v.IdPoolSize, time.Since(start).Seconds())
+	router := gin.Default()
+	router.Handle(http.MethodPost, "/run", func(c *gin.Context) {
+		for k, v := range testScenarios {
+			utils.Seed(c, ch, v.MessageVolume, v.IdPoolSize)
 
-		result := test.Execute(ch, v.Partitions, v.MessageVolume)
-		fmt.Printf("Complete: %v, Ordered: %v, Unique: %v, Partitioned: %v", result.DataComplete, result.DataOrdered, result.DataUniqe, result.DataProperlyPartitioned)
-	}
+			start := time.Now()
+			v.ExecuteScenarioFn(ch)
+			fmt.Printf("Executed scenario: %s, with %v messages and %v of ID pool size. In %v", k, v.MessageVolume, v.IdPoolSize, time.Since(start).Seconds())
+
+			result := test.Execute(ch, v.Partitions, v.MessageVolume)
+			c.JSON(http.StatusOK, map[string]any{
+				"complete": result.DataComplete, "ordered": result.DataOrdered, "unique": result.DataUniqe, "partitioned": result.DataProperlyPartitioned,
+			})
+		}
+		c.Status(200)
+	})
+	go func() {
+		if err := router.Run("0.0.0.0:8080"); err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	interruptChan := make(chan os.Signal)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-interruptChan
 }
