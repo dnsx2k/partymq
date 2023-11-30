b72b5a04bb8c65b1b26263df5ed7f5f472248aaf
Mateusz Pasterny <mpasterny@g2a.com>
2023-11-30 17:33:07 +0100

    Playing with Github actions and docker in docker

--- tests/main.go
+++ tests/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/dnsx2k/partymq/tests/pkg/scenarios"
 	"github.com/dnsx2k/partymq/tests/pkg/test"
 	"github.com/dnsx2k/partymq/tests/pkg/utils"
-	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -20,6 +17,7 @@ var testScenarios = map[string]utils.Scenario{
 }
 
 func main() {
+	time.Sleep(5 * time.Second)
 	amqpCs := os.Getenv("RABBIT_CS")
 
 	conn, err := amqp.Dial(amqpCs)
@@ -35,30 +33,15 @@ func main() {
 	}
 	utils.InitEmitterResources(ch)
 
-	router := gin.Default()
-	router.Handle(http.MethodPost, "/run", func(c *gin.Context) {
-		for k, v := range testScenarios {
-			utils.Seed(c, ch, v.MessageVolume, v.IdPoolSize)
+	c := context.Background()
+	for k, v := range testScenarios {
+		utils.Seed(c, ch, v.MessageVolume, v.IdPoolSize)
 
-			start := time.Now()
-			v.ExecuteScenarioFn(ch)
-			fmt.Printf("Executed scenario: %s, with %v messages and %v of ID pool size. In %v", k, v.MessageVolume, v.IdPoolSize, time.Since(start).Seconds())
+		start := time.Now()
+		v.ExecuteScenarioFn(ch)
+		fmt.Printf("Executed scenario: %s, with %v messages and %v of ID pool size. In %v", k, v.MessageVolume, v.IdPoolSize, time.Since(start).Seconds())
 
-			result := test.Execute(ch, v.Partitions, v.MessageVolume)
-			c.JSON(http.StatusOK, map[string]any{
-				"complete": result.DataComplete, "ordered": result.DataOrdered, "unique": result.DataUniqe, "partitioned": result.DataProperlyPartitioned,
-			})
-		}
-		c.Status(200)
-	})
-	go func() {
-		if err := router.Run("0.0.0.0:8080"); err != nil {
-			fmt.Println(err.Error())
-		}
-	}()
-
-	interruptChan := make(chan os.Signal)
-	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	<-interruptChan
+		result := test.Execute(ch, v.Partitions, v.MessageVolume)
+		fmt.Printf("Complete: %v, Ordered: %v, Unique: %v, Partitioned: %v", result.DataComplete, result.DataOrdered, result.DataUniqe, result.DataProperlyPartitioned)
+	}
 }
